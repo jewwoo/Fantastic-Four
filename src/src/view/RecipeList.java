cacd6fd7885430e6148ebae4f9f739c4e0c80b38
Jiho <tlswlgh0801@gmail.com>
2023-11-28 16:28:17 -0500

    added view for showing the list of recipes

--- src/src/view/RecipeList.java
+++ src/src/view/RecipeList.java
@@ -1,9 +1,35 @@
 package view;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class RecipeList {
+public class RecipeList extends JFrame{
 
 
     private JLabel lbResultNum;
+    private JList listRecipe;
+    private JButton btnOK;
+    private JPanel pnRecipe;
+    private JTextField tfInput;
+    private JPanel listPanel;
+
+    public RecipeList(){
+        setContentPane(listPanel);
+        setTitle("Result");
+        setSize(450, 600);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setVisible(true);
+        btnOK.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String input = tfInput.getText();
+                System.out.println(input);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        RecipeList recipeList = new RecipeList();
+    }
 }
