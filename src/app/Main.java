f9588cd47bd9ffa175d44b9ee913f1b337caa82d
Chelsea <chelseack.wong@mail.utoronto.ca>
2023-12-02 19:32:41 -0500

    Main

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,4 +1,19 @@
 package app;
 
+import view.*;
+
 public class Main {
+    public static void main(String[] args) {
+        // Run the SearchForm
+        javax.swing.SwingUtilities.invokeLater(() -> {
+            SearchForm searchForm = new SearchForm();
+            searchForm.setSearchCompleteListener(recipeData -> {
+                // Assuming you have the RecipeList implementation
+                // Create an instance of RecipeList and display it
+                RecipeList recipeList = new RecipeList(recipeData);
+                recipeList.setDefaultCloseOperation(javax.swing.JFrame.DISPOSE_ON_CLOSE);
+                recipeList.setVisible(true);
+            });
+        });
+    }
 }
