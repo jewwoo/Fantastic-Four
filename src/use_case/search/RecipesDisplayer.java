7e3d1ec4ac62ee970522b98dea88038f5d8d44db
chelseack <144476339+chelseack@users.noreply.github.com>
2023-12-13 00:10:39 -0500

    Update RecipesDisplayer.java

--- src/use_case/search/RecipesDisplayer.java
+++ src/use_case/search/RecipesDisplayer.java
@@ -15,49 +15,10 @@ public class RecipesDisplayer {
         int index = SearchFormController.getIndex();
 
         RecipeList recipeList = new RecipeList(null);
-        //if (arr_length == 1) {
-        //    System.out.println(arr_length + " recipes was found.");
-        //} else if (arr_length >= 100) {
-        //    System.out.println("100 recipes or more than 100 recipes were found");
-        //    arr_length = 100;
-        //} else {
-        //    System.out.println(arr_length + " recipes were found.");
-        //}
         
         index = displayRecipeLabels(data, index, arr_length, recipeList);
         SearchFormController.setindex(index);
     }    
-
-    //public static void DisplayRecipesFurther() {
-
-    //    boolean findOtherRecipes = true;
-
-    //    while (findOtherRecipes) {
-    //        System.out.println("Input recipe number or\n\t(n)ext to display next 10 recipes\n\t(p)revious to display previous 10 recipes\n\t(q)uit to find other recipes");
-
-    //        String select = Controller.getUserChoice();
-
-    //        switch (select.toLowerCase()) {
-    //            case "q":
-    //                findOtherRecipes = false;
-    //                break;
-    //            case "n":
-    //                index = displayRecipeLabels(data, index, arr_length, recipeList);
-    //                break;
-    //            case "p":
-    //                index = index - 20;
-    //                if (index < 0) {
-    //                    index = 0;
-    //                }
-    //                index = displayRecipeLabels(data, index, arr_length, recipeList);
-    //                break;
-    //            default:
-    //                int selection = Integer.parseInt(select);
-    //                displayRecipeDetails(selection);
-    //                break;
-    //        }
-    //    }
-    //}
 
     public static int displayRecipeLabels(JSONObject data, int index, int arr_length, RecipeList recipeList) {
         //System.out.println();
@@ -81,20 +42,8 @@ public class RecipesDisplayer {
                 index++;
             }
         }
-        //System.out.println();
         // Update the RecipeList GUI with the recipes
         recipeList.updateRecipeList(recipes);
         return index;
     }
-
-    //public static void displayRecipeDetails(int selectedIndex) {
-    //    ArrayList<String> hitsArray = Controller.gethit();
-    //    if (selectedIndex >= 0 && selectedIndex < hitsArray.size()) {
-    //        String selectedRecipe = hitsArray.get(selectedIndex);
-    //        // Assuming you want to print the details to the console for now
-    //        System.out.println("Details for Recipe #" + (selectedIndex + 1) + ": " + selectedRecipe);
-    //    } else {
-    //        System.out.println("Invalid index. Please enter a valid recipe index.");
-    //    }
-    //}
 }
